2898cf38f6d7a004571d57a10c1e73b4e448bef3
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-02 09:32:15 +0500

    well

--- if05.py
+++ if05.py
@@ -8,4 +8,15 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+    b = 0
+    if a<0:
+        b+=1
+    if b<0:
+        b+=1
+    if c<0:
+        b+=1
+    return b
+
+
+v= main(3,-3,-6)
+print(v)
